4c8be10e9a62b3a1459a3381b3aee68cdd0a527e
kuznikat <kuznikat@fel.cvut.cz>
2025-12-14 23:02:01 +0100

    header and footer

---
+++ about.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <!-- font link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <!--swiper library-remove later-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css"/>
+</head>
+<body>
+<!--header start-->
+<section class="header">
+    <a href="home.php" class="logo">Let's Travel</a>
+    <nav class="navbar">
+        <a href="home.php">Home</a>
+        <a href="about.php">About Us</a>
+        <a href="package.php">Package</a>
+        <a href="book.php">Book</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars fa-2x"></div>
+</section>
+<!--header end-->
+
+
+
+
+
+
+
+
+
+
+<!--footer start-->
+
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>Quick access links</h3>
+            <a href="home.php"><i class="fas fa-angle-right"></i>Home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i>About Us</a>
+            <a href="package.php"><i class="fas fa-angle-right"></i>Package</a>
+            <a href="book.php"><i class="fas fa-angle-right"></i>Book</a>
+
+        </div>
+
+        <div class="box">
+            <h3>Extra links</h3>
+            <a href="#"><i class="fas fa-angle-right"></i>Ask questions</a>
+            <a href="#"><i class="fas fa-angle-right"></i>About Us</a>
+            <a href="#"><i class="fas fa-angle-right"></i>Privacy Policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i>Terms of use</a>
+        </div>
+
+        <div class="box">
+            <h3>Contact info</h3>
+            <a href="#"><i class="fas fa-phone"></i>+420 777-666-555</a>
+            <a href="#"><i class="fas fa-envelope"></i> letstravel@gmail.com</a>
+            <a href="#"><i class="fas fa-map"></i> Prague, Czech Republic - Vinohradska 1, 120 00</a>
+        </div>
+
+        <div class="box">
+            <h3>Follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+            <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+            <a href="#"><i class="fab fa-tiktok"></i>Tiktok</a>
+        </div>
+    </div>
+
+
+    <div class="credit">created by: <span>Kateryna Kuznietsova </span>| all rights are reserved</div>
+</section>
+
+
+
+
+
+
+
+<!--footer end-->
+
+
+
+
+
+<!--swiper js - remove later-->
+<script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
+
+<!-- js file link -->
+<script src="script.js"></script>
+</body>
+</html>
