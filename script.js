a615e4e2fd79b7c6533c376a1d8c249a6670391d
kuznikat <kuznikat@fel.cvut.cz>
2026-01-09 12:33:08 +0100

    cleaning

--- script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menu-btn');
-let navigationBar = document.querySelector('.header .navbar');
-
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navigationBar.classList.toggle('active');
-}
